65a4c8fd1acbf77e3ed02fbd04b3db52a0225489
Lázaro <lazaroyaesta@gmail.com>
2024-03-01 19:13:37 +0100

    feat: implementing a quick server-side rendering trial

--- src/app/pages/comprar/components/page-comprar.component.ts
+++ src/app/pages/comprar/components/page-comprar.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-page-comprar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './page-comprar.component.html',
   styleUrl: './page-comprar.component.css',
 })
-export class PageComprarComponent {}
+export class PageComprarComponent {
+  private readonly http = inject(HttpClient);
+
+  todos$ = this.http
+    .get<{ userId: string; title: string }[]>(
+      'https://jsonplaceholder.typicode.com/todos'
+    )
+    .pipe(
+      tap((res) => {
+        console.log('Cargado desde el template, Todos los TODOS ', res);
+      })
+    );
+
+  constructor() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((res) => {
+        console.log('Cargado desde el constructor, todo 1 => ', res);
+      });
+  }
+
+  getTodo() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((res) => {
+        console.log('Cargado desde el boton, todo 1 => ', res);
+      });
+  }
+}
